a64951b91379f292d9e5e50e3f7141e012afa186
Italo Git <sulamita.italo12@gmail.com>
2025-06-24 17:59:16 -0300

    ajuste no modelo de email

--- src/routes/mail.ts
+++ src/routes/mail.ts
@@ -1,35 +1,24 @@
 import { FastifyInstance } from 'fastify';
-import nodemailer from 'nodemailer';
+import { transporter } from '../utils/mail';
+import { readFileSync } from 'fs';
 
 export async function mailRoutes(server: FastifyInstance) {
-  server.post('/send-email', async (request, reply) => {
-    const { to, subject, html } = request.body as {
-      to: string;
-      subject: string;
-      html: string;
-    };
+    server.post('/send-email', async (request, reply) => {
 
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-      },
-      connectionTimeout: 10000
+        const meuHtml = readFileSync('./templates/email.html', 'utf-8');
+
+        try {
+            const info = await transporter.sendMail({
+                from: process.env.MAIL_USER,
+                to: "italomleitez@gmail.com",
+                subject: "Aviso",
+                html: meuHtml
+            });
+
+            return reply.send({ success: true, info });
+        } catch (error) {
+            console.error(error);
+            return reply.status(500).send({ success: false, error });
+        }
     });
-
-    try {
-      const info = await transporter.sendMail({
-        from: process.env.EMAIL,
-        to,
-        subject,
-        html
-      });
-
-      return reply.send({ success: true, info });
-    } catch (error) {
-      console.error(error);
-      return reply.status(500).send({ success: false, error });
-    }
-  });
 }
