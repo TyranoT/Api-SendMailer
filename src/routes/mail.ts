22c2f41ab5146933693f02bc1c2ce4f7100cae63
Italo Git <sulamita.italo12@gmail.com>
2025-07-06 14:52:07 -0300

    Update mail.ts

--- src/routes/mail.ts
+++ src/routes/mail.ts
@@ -1,18 +1,19 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyRequest } from 'fastify';
 import { transporter } from '../utils/mail';
 import { readFileSync } from 'fs';
 import path from 'path';
 
 export async function mailRoutes(server: FastifyInstance) {
-    server.get('/send-email', async (request, reply) => {
+    server.post('/send-email', async (request, reply) => {
 
         const htmlPath = path.join(__dirname, 'templates', 'mail.html');
         const meuHtml = readFileSync(htmlPath, 'utf-8');
+        const { email } = request.body as { email: string };
 
         try {
             const info = await transporter.sendMail({
                 from: process.env.MAIL_USER,
-                to: "italomleitez@gmail.com",
+                to: email,
                 subject: "Aviso",
                 html: meuHtml
             });
